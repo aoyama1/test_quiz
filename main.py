c5d38cf605cbeecd40ded5a982da338ca5954c98
aoyama <aoyama>
2020-05-09 22:31:30 +0900

    first

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', \
         title="Welcome to Rope_Blog",\
-        message="Who are you?")
+        message="織田信長が今川義元を打ち取った戦いの名は？",\
+    )  
 
 @app.route('/', methods=['POST'])
 def form():
-    field = request.form['field']
-    return render_template('index.html',\
-        title="Sample answer",\
-            message="Hello %s-san" % field)
-
+    flg= 'rd'
+    rd = request.form.get('radio')
+    return render_template('index2_post.html',\
+    flg=rd)     
 if __name__ == '__main__':
     app.debug=True
-    app.run()
+    app.run(),
+
 
 #def hello_world():
 #    return 'welcome to Flask World!'
 #if __name__ == '__main__':
 #    app.debug=True
 #    app.run(host='localhost')
+
+#@app.route('/', methods=['POST'])
+#def form():
+#    field = request.form['field']
+#    return render_template('index.html',\
+#        title="Sample answer",\
+#            message="Hello %s-san" % field)
